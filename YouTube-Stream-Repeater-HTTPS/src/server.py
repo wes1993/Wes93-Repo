8e953b1396d8565855231be4166262855c980253
wes1993 <52070589+wes1993@users.noreply.github.com>
2023-02-02 15:29:56 +0100

    Update server.py

--- YouTube-Stream-Repeater-HTTPS/src/server.py
+++ YouTube-Stream-Repeater-HTTPS/src/server.py
@@ -2,10 +2,13 @@ import mimetypes
 import os
 
 import magic
+import secrets
 
-from fastapi import FastAPI, HTTPException
+
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.responses import StreamingResponse, JSONResponse, FileResponse
 from fastapi.background import BackgroundTasks
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from util.sub import download_subs
 from util.meta import query_meta
@@ -13,7 +16,30 @@ from util.stream import stream_from_yt
 
 app = FastAPI()
 
+security = HTTPBasic()
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = b"stanleyjobson"
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = b"swordfish"
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
 @app.get("/dl/{video_id}")
+def read_current_user(username: str = Depends(get_current_username)):
+    return {"username": username}
 async def api_dl(
     video_id: str,   # the video's ID (watch?v=<this>)
     f: str = "best", # format 
