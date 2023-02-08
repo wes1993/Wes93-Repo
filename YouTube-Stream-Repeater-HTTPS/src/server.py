949beac9dfa5baa597d06392d4098bb2cd4344c6
wes1993 <52070589+wes1993@users.noreply.github.com>
2023-02-08 15:27:35 +0100

    Update server.py

--- YouTube-Stream-Repeater-HTTPS/src/server.py
+++ YouTube-Stream-Repeater-HTTPS/src/server.py
@@ -26,12 +26,12 @@ security = HTTPBasic()
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = bytes('user')
+    correct_username_bytes = bytes('user', 'utf-8')
     is_correct_username = secrets.compare_digest(
         current_username_bytes, correct_username_bytes
     )
     current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = bytes('password')
+    correct_password_bytes = bytes('password', 'utf-8')
     is_correct_password = secrets.compare_digest(
         current_password_bytes, correct_password_bytes
     )
